31b9252e64dc3307ce3e6bfe479bd8159d50e4c4
nik <khamdamov_1996@mail.ru>
2022-11-06 11:40:45 +0500

    add settings and create jome page

--- public/index.php
+++ public/index.php
@@ -3,14 +3,27 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
+use DI\Container;
+
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+    return $this->get("renderer")->render($response, "url/index.phtml");
+});
+
+$app->get('/{id}', function (Request $request, Response $response, $args) {
+    $id = $args["id"];
+    return $this->get("renderer")->render($response, "url/index.phtml", ["id" => $id]);
 });
 
 $app->run();
